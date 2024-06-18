e0991d21caf640c380b7cab7b8dacf9ffdf3f51b
Dhafindra <Dhafindra.razaqa@gmail.com>
2024-06-19 06:52:02 +0700

    made the video bigger

--- resources/views/watch.blade.php
+++ resources/views/watch.blade.php
@@ -16,8 +16,8 @@
             align-items: center;
         }
         .container {
-            width: 45%; /* Adjust the width as needed */
-            height: 60%;
+            width: 95%; /* Adjust the width as needed */
+            height: 80%;
             position: absolute; /* Position it absolutely within the body */
             top: 50%; /* Position the top edge of the container at the middle of the page */
             left: 50%; /* Position the left edge of the container at the middle of the page */
@@ -32,7 +32,7 @@
         }
         iframe {
             width: 100%;
-            height: 450px;
+            height: 90%;
             border: none;
             border-radius: 8px;
         }
@@ -44,25 +44,47 @@
             border-radius: 5px;
             cursor: pointer;
         }
-        .back-button {
-            background-color: #dc3545;
+        /* .back-button {
+            margin: 20px auto;
+            background-color: #dc3645;
             border-radius: 5px;
-        }
+            font-family: 'Lato', Arial, sans-serif;
+        } */
         button:hover{
             background-color: #0056b3; /* Darker shade of blue */
         }
-        .back-button:hover{
+        /* .back-button:hover{
             background-color: #a91b0d;
+        } */
+        .back-button {
+            position: absolute; /* Change to absolute positioning */
+            top: 10px;
+            left: 10px;
+            padding: 5px 15px; /* Adjust padding */
+            background-color: #888; /* Set to a slightly more gray color */
+            color: black;
+            text-align: center;
+            border-radius: 15px;
+            text-decoration: none;
+            font-family: 'Lato', Arial, sans-serif;
+            display: inline-block;
+            font-size: 24px; /* Adjust font size */
+            opacity: 50%;
+            margin: 30px 28px;
+        }
+        .back-button:hover {
+            background-color: #666;
         }
     </style>
 </head>
 <body>
     <div class="container">
+        <a href="{{ route('home') }}" class="back-button">&lt;</a>
         <h2>{{ $course->videoTitle }}</h2>
         <iframe src="{{ $videoUrl }}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <a href="{{ route('home') }}" class="back-button">
+        <!-- <a href="{{ route('home') }}" class="back-button">
             <button type="button" class="back-button">Back to Home</button>
-        </a>
+        </a> -->
     </div> 
 </body>
 </html>
